24bbc3dee60e21a372956bc82f641c4c8ea09c60
Anirud Shrestha <anirudshrestha@boisestate.edu>
2022-09-21 21:26:14 -0600

    setting up user class

---
+++ MyDay.Api/Features/MyDayUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyDay.Api.Features
+{
+	public class MyDayUser
+	{
+		public string AppUserID { get; set; } = string.Empty;
+		public string UserName { get; set; } = string.Empty;
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public DateTime Joined { get; set; }
+		public int MyDayPosts { get; set; }
+		public int Journals { get; set; }
+		public string City { get; set; } = string.Empty;
+		public List<string> Links { get; set; } = new List<string>();
+		public string Description { get; set; } = string.Empty;
+	}
+}
+
