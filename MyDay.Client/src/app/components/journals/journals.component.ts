99b610668626180ec87429c2c7510095457bc14d
Anirud Shrestha <anirudshrestha@boisestate.edu>
2022-09-25 10:52:10 -0600

    add: journal comment functionaliy setup

--- MyDay.Client/src/app/components/journals/journals.component.ts
+++ MyDay.Client/src/app/components/journals/journals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Event as NavigationEvent, NavigationStart } from '@angular/router';
 import { faUserCircle, faShare, faHeart, faComment} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -11,9 +12,37 @@ export class JournalsComponent implements OnInit {
   faShare = faShare;
   faHeart = faHeart;
   faComment = faComment;
-  constructor() { }
+  constructor(private router:Router) {
+    this.router.events
+    .subscribe(
+      (event: NavigationEvent) => {
+        if(event instanceof NavigationStart) {
+          this.checkCommentDialogue()
+        }
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  popCommentDialogue(e){
+    const cmtBox = e.target.parentElement.parentElement.parentElement.parentElement.childNodes[0];
+    if(cmtBox.classList.contains('normal')) cmtBox.classList.remove('normal')
+    cmtBox.classList.toggle('reveal')
+    const backdrop = document.querySelector('.backdrop')
+    if(!backdrop.classList.contains('reveal')) backdrop.classList.add('reveal')
+    backdrop.addEventListener('click', () => {
+      cmtBox.classList.remove('reveal')
+      backdrop.classList.remove('reveal')
+      backdrop.removeAllListeners('click')
+    })
+  }
+
+  checkCommentDialogue(){
+    const cmtBox = document.querySelectorAll('.comment-dialogue');
+    cmtBox.forEach(box => {
+      if(box.classList.contains('reveal')) box.classList.replace('reveal', 'normal')
+    })
   }
 
 }
