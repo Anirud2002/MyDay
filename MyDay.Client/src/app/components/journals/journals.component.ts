7aaaee912aa1dbf00437e88d29d1df1287e4ed4e
Anirud Shrestha <anirudshrestha@boisestate.edu>
2022-10-18 22:53:26 -0600

    refactor: consistent changes on journal route

--- MyDay.Client/src/app/components/journals/journals.component.ts
+++ MyDay.Client/src/app/components/journals/journals.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, Event as NavigationEvent, NavigationStart } from '@angular/router';
+import { Router } from '@angular/router';
 import { faUserCircle, faShare, faHeart, faComment, faChevronDown} from '@fortawesome/free-solid-svg-icons';
+import { PostReponse } from '../../_interfaces/post-response.modal';
+import { PostService } from '../../_services/post.service';
 
 @Component({
   selector: 'app-journals',
@@ -13,39 +15,17 @@ export class JournalsComponent implements OnInit {
   faHeart = faHeart;
   faComment = faComment;
   faChevronDown = faChevronDown;
+  posts: PostReponse[] = [];
   activeJournalCategory: string = 'Motivating';
   journalCategories: string[] = ['Motivating', 'Grateful', 'Sad']
-  constructor(private router:Router) {
-    this.router.events
-    .subscribe(
-      (event: NavigationEvent) => {
-        if(event instanceof NavigationStart) {
-          this.checkCommentDialogue()
-        }
-    });
-   }
+  constructor(private router:Router, private postService: PostService) { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.getPosts();
   }
 
-  popCommentDialogue(e){
-    const cmtBox = e.target.parentElement.parentElement.childNodes[0];
-    if(cmtBox.classList.contains('normal')) cmtBox.classList.remove('normal')
-    cmtBox.classList.toggle('reveal')
-    const backdrop = document.querySelector('.backdrop')
-    if(!backdrop.classList.contains('reveal')) backdrop.classList.add('reveal')
-    backdrop.addEventListener('click', () => {
-      cmtBox.classList.remove('reveal')
-      backdrop.classList.remove('reveal')
-      backdrop.removeAllListeners('click')
-    })
-  }
-
-  checkCommentDialogue(){
-    const cmtBox = document.querySelectorAll('.comment-dialogue');
-    cmtBox.forEach(box => {
-      if(box.classList.contains('reveal')) box.classList.replace('reveal', 'normal')
-    })
+  async getPosts(){
+    this.posts = await this.postService.getPosts("journal") as PostReponse[];
   }
 
   toggleDropdown(){
