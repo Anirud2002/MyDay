beee789217608659803e732b9385186a9bae1cad
Anirud Shrestha <anirudshrestha@boisestate.edu>
2022-11-09 22:57:55 -0700

    add: edit and cancel button on inputs

--- MyDay.Client/src/app/components/profile/components/info/info.component.ts
+++ MyDay.Client/src/app/components/profile/components/info/info.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faArrowRightFromBracket, faPencil } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightFromBracket, faPencil, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { UserDetails } from '../../../../_interfaces/user-details.modal';
 import { User } from '../../../../_interfaces/user.modal';
 import { AccountService } from '../../../../_services/account.service';
@@ -15,6 +15,7 @@ export class InfoComponent implements OnInit {
   userDetails: UserDetails;
   faArrowRightFromBracket = faArrowRightFromBracket;
   faPencil = faPencil;
+  faXmark = faXmark;
   
   // boolean to check if user has hovered over the input and show the edit button
   showEditBtn: boolean = false;
@@ -22,6 +23,11 @@ export class InfoComponent implements OnInit {
   nameEdit: boolean = false;
   cityEdit: boolean = false;
   descEdit: boolean = false;
+
+  // booleans to show or hide the edit buttons for each input
+  showNameEditBtn: boolean = false;
+  showCityEditBtn: boolean = false;
+  showDescEditBtn: boolean = false;
 
   profileForm: FormGroup;
   constructor(private accountService: AccountService, private fb: FormBuilder) { }
@@ -42,18 +48,51 @@ export class InfoComponent implements OnInit {
     this.userDetails = await this.accountService.getUserDetails(this.user.userName) as UserDetails;
   }
 
+  doSmth(){
+    this.nameEdit = false;
+  }
+
   handleInputEdit(type: string){
     switch(type){
       case "name": {
         this.nameEdit = true;
+        this.profileForm.patchValue({fullName: ""})
         break;
       }
       case "city": {
         this.cityEdit = true;
+        this.profileForm.patchValue({city: ""})
         break;
       }
       case "desc": {
         this.descEdit = true;
+        this.profileForm.patchValue({description: ""})
+        break;
+      }
+    }
+  }
+
+  handleClearInput(type: string){
+    switch(type){
+      case "name": {
+        if(!(this.profileForm.get("fullName").value as string)){
+          this.nameEdit = false;
+        }
+        this.profileForm.patchValue({fullName: ""})
+        break;
+      }
+      case "city": {
+        if(!(this.profileForm.get("city").value as string)){
+          this.cityEdit = false;
+        }
+        this.profileForm.patchValue({city: ""})
+        break;
+      }
+      case "desc": {
+        if(!(this.profileForm.get("description").value as string)){
+          this.descEdit = false;
+        }
+        this.profileForm.patchValue({description: ""})
         break;
       }
     }
