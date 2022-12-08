083d8919138ed55022dc539410b3ba5294bddebd
Anirud Shrestha <anirudshrestha@boisestate.edu>
2022-12-08 10:35:52 -0700

    refactor: profile and support page UI

--- MyDay.Client/src/app/components/profile/components/info/info.component.ts
+++ MyDay.Client/src/app/components/profile/components/info/info.component.ts
@@ -3,7 +3,7 @@ import { faArrowRightFromBracket, faPencil, faXmark } from '@fortawesome/free-so
 import { UserDetails } from '../../../../_interfaces/user-details.modal';
 import { User } from '../../../../_interfaces/user.modal';
 import { AccountService } from '../../../../_services/account.service';
-import { FormGroup, FormBuilder } from  '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from  '@angular/forms';
 
 @Component({
   selector: 'app-info',
@@ -16,87 +16,28 @@ export class InfoComponent implements OnInit {
   faArrowRightFromBracket = faArrowRightFromBracket;
   faPencil = faPencil;
   faXmark = faXmark;
+  name: string;
+  city: string;
+  description: string;
+  joined: Date;
   
-  // boolean to check if user has hovered over the input and show the edit button
-  showEditBtn: boolean = false;
-
-  nameEdit: boolean = false;
-  cityEdit: boolean = false;
-  descEdit: boolean = false;
-
-  // booleans to show or hide the edit buttons for each input
-  showNameEditBtn: boolean = false;
-  showCityEditBtn: boolean = false;
-  showDescEditBtn: boolean = false;
-
-  profileForm: FormGroup;
   constructor(private accountService: AccountService, private fb: FormBuilder) { }
 
   async ngOnInit() {
-    await this.getUserDetails();
-    this.profileForm = this.fb.group({
-      fullName: [''],
-      city: [''],
-      description: [''],
-    })
+    this.userDetails = await this.getUserDetails();
+    this.name = this.userDetails.firstName + " " + this.userDetails.lastName;
+    this.city = this.userDetails.city;
+    this.description = this.userDetails.description;
+    this.joined = this.userDetails.joined;
   }
 
   async getUserDetails(){
     this.accountService.currentUser$.subscribe(user => {
       this.user = user;
     })
-    this.userDetails = await this.accountService.getUserDetails(this.user.userName) as UserDetails;
+    return await this.accountService.getUserDetails(this.user.userName) as UserDetails;
   }
 
-  doSmth(){
-    this.nameEdit = false;
-  }
-
-  handleInputEdit(type: string){
-    switch(type){
-      case "name": {
-        this.nameEdit = true;
-        this.profileForm.patchValue({fullName: ""})
-        break;
-      }
-      case "city": {
-        this.cityEdit = true;
-        this.profileForm.patchValue({city: ""})
-        break;
-      }
-      case "desc": {
-        this.descEdit = true;
-        this.profileForm.patchValue({description: ""})
-        break;
-      }
-    }
-  }
-
-  handleClearInput(type: string){
-    switch(type){
-      case "name": {
-        if(!(this.profileForm.get("fullName").value as string)){
-          this.nameEdit = false;
-        }
-        this.profileForm.patchValue({fullName: ""})
-        break;
-      }
-      case "city": {
-        if(!(this.profileForm.get("city").value as string)){
-          this.cityEdit = false;
-        }
-        this.profileForm.patchValue({city: ""})
-        break;
-      }
-      case "desc": {
-        if(!(this.profileForm.get("description").value as string)){
-          this.descEdit = false;
-        }
-        this.profileForm.patchValue({description: ""})
-        break;
-      }
-    }
-  }
 
   logout(){
     this.accountService.logout();
