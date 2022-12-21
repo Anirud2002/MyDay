38d75af1d110feb423529674c090bd843b7f3775
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2022-12-20 23:25:56 -0700

    refactor: test cases

---
+++ MyDay.Tests/ControllerTests/PostControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using MyDay.Api.Controllers;
+using MyDay.Api.DTOs;
+using MyDay.Api.Entities;
+using Shouldly;
+using Xunit;
+
+namespace MyDay.Tests.ControllerTests
+{
+    public class PostControllerTests
+    {
+        private PostController _postController;
+        private IDynamoDBContext _dynamoDBContext;
+        public PostControllerTests()
+        {
+            // Dependencies
+            _dynamoDBContext = A.Fake<IDynamoDBContext>();
+
+            // SUT -> System Under Test
+            _postController = new PostController(_dynamoDBContext);
+        }
+
+        [Fact]
+        public void PostController_GetPosts_ReturnSuccess()
+        {
+            // Arrage - What do I need to bring in?
+            var posts = A.Fake<Task<ActionResult>>();
+            A.CallTo(() => _postController.GetPosts("MYDAY")).Returns(posts);
+
+            // Act
+            var result = _postController.GetPosts("MYDAY");
+
+            // Assert
+            result.ShouldBeOfType<Task<ActionResult>>();
+
+        }
+    }
+}
+
