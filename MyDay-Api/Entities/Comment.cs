aae502627781e40775163c3a740047c4b3a63fa7
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-02-12 14:24:03 -0700

    adding username in comment class

--- MyDay-Api/Entities/Comment.cs
+++ MyDay-Api/Entities/Comment.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 namespace MyDay.Api.Entities
 {
-	public class Comment
-	{
-		// When someone posts a comment, the DB queries that particular post or journal
-		// and then updates the comments list my adding the new comment with this
-		// information
-		public string AppUserID { get; set; } = string.Empty;
-		public string FirstName { get; set; } = string.Empty;
-		public string LastName { get; set; } = string.Empty;
-		public DateTime CommentedOn { get; set; }
-		public string CommentBody { get; set; } = string.Empty;
-	}
+    public class Comment
+    {
+        // When someone posts a comment, the DB queries that particular post or journal
+        // and then updates the comments list my adding the new comment with this
+        // information
+        public string AppUserID { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime CommentedOn { get; set; }
+        public string CommentBody { get; set; } = string.Empty;
+    }
 }
 
--- MyDay-Api/Extensions/ClaimsPrincipalExtension.cs
+++ MyDay-Api/Extensions/ClaimsPrincipalExtension.cs
@@ -3,12 +3,12 @@ using System.Security.Claims;
 
 namespace MyDay.Api.Extensions
 {
-	public static class ClaimsPrincipalExtension
-	{
-		public static string GetUserName(this ClaimsPrincipal user)
+    public static class ClaimsPrincipalExtension
+    {
+        public static string? GetUserName(this ClaimsPrincipal user)
         {
-			return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
         }
-	}
+    }
 }
 
