e1c6f1af6991b5f38f9dc3b01a4d59e23e05b399
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-03-08 23:49:17 -0700

    some ui for photo uploader

--- MyDay-Client/src/app/components/profile/components/upload-main-pic/upload-main-pic.component.ts
+++ MyDay-Client/src/app/components/profile/components/upload-main-pic/upload-main-pic.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faUpload } from '@fortawesome/free-solid-svg-icons';
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-upload-main-pic',
@@ -8,10 +9,41 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class UploadMainPicComponent implements OnInit {
   @Output() closeModal = new EventEmitter();
+  uploader: FileUploader;
+  hasBaseDropzoneOver = false;
+  
   faTimes = faTimes;
+  faUpload = faUpload;
   constructor() { }
 
   ngOnInit(): void {
+    this.initFileUploader();
+  }
+
+  initFileUploader(){
+    this.uploader = new FileUploader({
+      url: "https://localhost:5001",
+      authToken: 'Bearer ',
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    })
+
+    this.uploader.onAfterAddingFile = file => {
+      file.withCredentials = false;
+    }
+
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response){
+        const photo = JSON.parse(response);
+      }
+    }
+  }
+
+  fileOverBase(e: any){
+    this.hasBaseDropzoneOver = e;
   }
 
   handleCloseModal(){
