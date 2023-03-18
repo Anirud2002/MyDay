e59b86a045f9a3f7002e410955556d1f6276a686
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-03-17 19:35:45 -0600

    cleanup

--- MyDayApi/LambdaFunction.cs
+++ MyDayApi/LambdaFunction.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-using Amazon.Lambda.Core;
-namespace MyDayApi;
+﻿namespace MyDayApi;
 
 //// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 //[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
